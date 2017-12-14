48244380b25f40ce8202c53081d836144f9d3d11
Ben Zarras <benzarras@gmail.com>
2017-12-14 18:37:19 -0500

    fix error

--- src/services/Coinbase.js
+++ src/services/Coinbase.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Promise = require('bluebird'),
+    CoinPrice = require('../CoinPrice'),
     Client = require('coinbase').Client,
     client = new Client({
         apiKey: process.env.COINBASE_KEY,
